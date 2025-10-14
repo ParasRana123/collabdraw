0b9628db9fae35ea7e5bc70186e5e3bd3c0e874e
ParasRana123 <parasrana579@gmail.com>
2025-10-14 12:10:07 +0530

    init

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+
+const app = express();
+
+app.post("/signup" , (req , res) => {
+
+})
+
+app.post("/signin" , (req , res) => {
+
+    const userId = 1;
+    const token = jwt.sign({
+        userId
+    } , JWT_SECRET);
+
+    return res.json({
+        token
+    })
+})
+
+app.post("/room" , (req , res) => {
+
+})
+
+app.listen(3000);

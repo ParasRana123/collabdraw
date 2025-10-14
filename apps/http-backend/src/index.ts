0777751f3c94f846d813fb03cc43ceb061fa75ec
ParasRana123 <parasrana579@gmail.com>
2025-10-14 15:37:53 +0530

    added zod as a package

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,32 +1,57 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import { middlware } from "./middleware";
-import { JWT_SECRET } from "@repo/backend-common/config"
+import { JWT_SECRET } from "@repo/backend-common/config";
+import { CreateRoomSchema, CreateSignInSchema, CreateUserSchema } from "@repo/common/types";
 
 const app = express();
 
-app.post("/signup" , (req , res) => {
-    // db-call
-})
-
-app.post("/signin" , (req , res) => {
-
-    const userId = 1;
-    const token = jwt.sign({
-        userId
-    } , JWT_SECRET);
-
+app.post("/signup", (req, res) => {
+  const data = CreateUserSchema.safeParse(req.body);
+  if (!data.success) {
     return res.json({
-        token
-    })
-})
+      message: "Incorrect inputs",
+    });
+  }
+  // db-call
+  res.json({
+    userId: "123",
+  });
+});
 
-app.post("/room" , middlware , (req , res) => {
-    // db-call
+app.post("/signin", (req, res) => {
+  const data = CreateSignInSchema.safeParse(req.body);
+  if (!data.success) {
+    return res.json({
+      message: "Incorrect inputs",
+    });
+  }
+  const userId = 1;
+  const token = jwt.sign(
+    {
+      userId,
+    },
+    JWT_SECRET
+  );
 
-    res.json({
-        roomId: "123"
-    })
-})
+  return res.json({
+    token,
+  });
+});
+
+app.post("/room", middlware, (req, res) => {
+
+  const data = CreateRoomSchema.safeParse(req.body);
+  if (!data.success) {
+    return res.json({
+      message: "Incorrect inputs",
+    });
+  }
+  // db-call
+
+  res.json({
+    roomId: "123",
+  });
+});
 
 app.listen(3000);

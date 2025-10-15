f0aa0f43a19e950cfc4e8938fd4185bf757e2ab5
ParasRana123 <parasrana579@gmail.com>
2025-10-16 00:44:01 +0530

    added the room and chats frontend

---
+++ apps/web/app/component/ChatRoom.tsx
@@ -0,0 +1,15 @@
+import { BACKEND_URL } from "../config"
+import axios from "axios";
+import { ChatRoomCient } from "./ChatRoomClient";
+
+async function getChats(roomId: string) {
+    const response = await axios.get(`${BACKEND_URL}/chats/${roomId}`);
+    return response.data.messages;
+}
+
+export async function ChatRoom({id} : {
+    id: string
+}) {
+    const messages = await getChats(id);
+    return <ChatRoomCient id={id} messages={messages} />
+}

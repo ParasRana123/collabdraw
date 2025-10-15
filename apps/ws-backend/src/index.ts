611c0dea6f4622a8f04000030aa5f8322df14c65
ParasRana123 <parasrana579@gmail.com>
2025-10-15 18:34:25 +0530

    http done starting ws

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -4,6 +4,20 @@ import { JWT_SECRET } from "@repo/backend-common/config";
 
 const wss = new WebSocketServer({ port: 8080 });
 
+function checkUser(token: string): string | null {
+  const decoded = jwt.verify(token , JWT_SECRET);
+
+  if(typeof decoded == "string") {
+    return null;
+  }
+
+  if(!decoded || !decoded.userId) {
+    return null;
+  }
+
+  return decoded.userId;
+}
+
 wss.on('connection', function connection(ws , request) {
   const url = request.url;
   if(!url) {
@@ -11,14 +25,12 @@ wss.on('connection', function connection(ws , request) {
   }
   const queryParams = new URLSearchParams(url.split('?')[1]);
   const token = queryParams.get('token') || "";
-
-  const decoded = jwt.verify(token , JWT_SECRET);
-
-  if(!decoded || !(decoded as JwtPayload).userId) {
+  const userId = checkUser(token);
+  if(!userId) {
     ws.close();
-    return;
   }
-
+  
+  
   ws.on('message', function message(data) {
     ws.send('pong');
   });

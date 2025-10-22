13125fb0f4299a6cf7a5a3641f8bfe06ff605d71
ParasRana123 <parasrana579@gmail.com>
2025-10-23 00:02:32 +0530

    added 2 shapes in frontend

--- apps/excalidraw-frontend/components/RoomCanvas.tsx
+++ apps/excalidraw-frontend/components/RoomCanvas.tsx
@@ -7,6 +7,7 @@ import { Canvas } from "./Canvas";
 export function RoomCanvas({ roomId } : {roomId: string}) {
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const [socket , setSocket] = useState<WebSocket | null>(null);
+    const [shape , setShape] = useState("null");
 
     useEffect(() => {
         const ws = new WebSocket(`${WS_URL}?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJkZTIyMTU5NS0wNTBlLTQ2N2UtODVhMS00NzNhMDA5MzliMWQiLCJpYXQiOjE3NjA3MjA3Mzl9.C9zPXmzY6DnyZ0O37RJzEI9VVEkp7I4Qe6h308apVmw`);
@@ -27,6 +28,25 @@ export function RoomCanvas({ roomId } : {roomId: string}) {
     }
 
     return <div>
-        <Canvas roomId={roomId} socket={socket}/>
+        <button style={{
+                padding: '8px 16px',
+                margin: '0 5px',
+                border: 'none',
+                borderRadius: '20px',
+                backgroundColor: shape === "rect" ? '#4CAF50' : '#555',
+                color: 'white',
+                cursor: 'pointer',
+                transition: 'background-color 0.3s'
+        }} onClick={() => { (shape !== "rect")? setShape("rect") : setShape("null")}}>Rectangle</button>
+        <button style={{
+                padding: '8px 16px',
+                margin: '0 5px',
+                border: 'none',
+                borderRadius: '20px',
+                backgroundColor: shape === "circle" ? '#4CAF50' : '#555',
+                color: 'white',
+                cursor: 'pointer',
+                transition: 'background-color 0.3s'
+        }} onClick={() => { (shape !== "circle")? setShape("circle") : setShape("null")}}>Circle</button>
     </div>
 }
